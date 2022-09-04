142bed55649bf7fbf44e86aa552dc5d81bb0370f
KimHyunmin <hmkim112@naver.com>
2022-09-04 18:18:52 +0900

    Update YouTube_Windows.py

--- YouTube_Windows.py
+++ YouTube_Windows.py
@@ -1,11 +1,8 @@
-from fileinput import filename
-from sre_parse import SPECIAL_CHARS
-from tkinter.tix import InputOnly
-from tokenize import Special
-from unicodedata import name
 from pytube import YouTube
 from pytube.cli import on_progress
 import os
+from time import sleep
+import sys
 
 name = input("YouTube url : ")
 yt_url = name
@@ -15,7 +12,7 @@ def youtube_download(file_url):
     yt = YouTube(file_url, on_progress_callback=on_progress)  # YouTube 객체 생성
 
     video_streams = yt.streams
-    print(video_streams)  # streams 처리 전체 정보
+   # print(video_streams)  # streams 처리 전체 정보
 
     print(f'==================================================================')
     print(f'* 제작자\t: {yt.author}')
@@ -30,17 +27,10 @@ def youtube_download(file_url):
     print(
         f'* 영상 길이\t: {str(yt.length // 60).zfill(2)}:{str(yt.length % 60).zfill(2)} / {yt.length} 초.')
     print(f'* 영상 조회수\t: {yt.views}')
-    print(f'==================================================================')
-    print(f'* 영상 키워드\t: {yt.keywords}\n')
-    print(f'* 영상 설명\n\n{yt.description}')
-    print(f'==================================================================')
-
-    """encoding 처리"""
+    print('==================================================================')
     video_streams = yt.streams.filter(file_extension='mp4').get_by_itag(22)
-                                                            #Youtube itag : https://gist.github.com/rlagusals1102/c4c1fb870afeccba06cc9633af8ca9fd"""
-    print(video_streams)
+    #print(video_streams)
 
-    """저장 폴더 생성"""
     if not os.path.exists('youtube_download'):
         os.mkdir("youtube_download")
     else:
@@ -59,5 +49,6 @@ def youtube_download(file_url):
 
     print('영상 다운로드 완료')
 
+    os.system("pause")
 
 youtube_download(yt_url)
